61c8494c5a86ba4ffcf7a0fb785358df6186080b
sqrt <sqrt@octoblu.com>
2016-02-03 12:39:58 -0700

    Convert to package format

--- etcdclient/etcdclient.go
+++ etcdclient/etcdclient.go
@@ -1,28 +1,28 @@
-package main
+package etcdclient
 
 import (
 	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/coreos/etcd/client"
 )
 
-// SimpleEtcdClient lets your Get/Set from Etcd
-type SimpleEtcdClient struct {
+// EtcdClient lets your Get/Set from Etcd
+type EtcdClient struct {
 	etcd client.Client
 }
 
-// New constructs a new SimpleEtcdClient
-func New(etcdURI string) (*SimpleEtcdClient, error) {
+// New constructs a new EtcdClient
+func New(etcdURI string) (*EtcdClient, error) {
 	etcd, err := client.New(client.Config{
 		Endpoints: []string{etcdURI},
 	})
 	if err != nil {
 		return nil, err
 	}
-	return &SimpleEtcdClient{etcd}, nil
+	return &EtcdClient{etcd}, nil
 }
 
 // Get gets a value in Etcd
-func (etcdClient *SimpleEtcdClient) Get(key string) (string, error) {
+func (etcdClient *EtcdClient) Get(key string) (string, error) {
 	api := client.NewKeysAPI(etcdClient.etcd)
 	response, err := api.Get(context.Background(), key, nil)
 	if err != nil {
@@ -35,7 +35,7 @@ func (etcdClient *SimpleEtcdClient) Get(key string) (string, error) {
 }
 
 // Set sets a value in Etcd
-func (etcdClient *SimpleEtcdClient) Set(key, value string) error {
+func (etcdClient *EtcdClient) Set(key, value string) error {
 	api := client.NewKeysAPI(etcdClient.etcd)
 	_, err := api.Set(context.Background(), key, value, nil)
 	return err
